1ba82b2f4cb9c95c1c3a0d205e1e5e74346dac8e
mac-haley <hongwei.huang42@gmail.com>
2025-08-03 19:16:40 +0800

    use ai to generate questions

--- internal/app/controllers/openai_controller.go
+++ internal/app/controllers/openai_controller.go
@@ -1,19 +1,26 @@
 package controllers
 
 import (
+	"QuizPals-Server/internal/app/services"
 	"github.com/gin-gonic/gin"
+	"io"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 type OpenAIController struct {
+	openaiServe services.OpenAIService
 }
 
-func NewUserController() *OpenAIController {
-	return &OpenAIController{}
+func NewUserController(OpenAIService services.OpenAIService) *OpenAIController {
+	return &OpenAIController{
+		openaiServe: OpenAIService,
+	}
 }
 
-func (ctrl *OpenAIController) UploadFile(c *gin.Context) {
+func (oc *OpenAIController) UploadFile(c *gin.Context) {
 	file, err := c.FormFile("mdFile")
 	if err != nil {
 		log.Println(err)
@@ -26,12 +33,58 @@ func (ctrl *OpenAIController) UploadFile(c *gin.Context) {
 	}
 	log.Println(file.Filename)
 
-	//err = c.SaveUploadedFile(file, file.Filename)
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext != ".md" {
+		log.Println("File type not supported", ext)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "Only supports Markdown files(.md, .markdown)",
+			"data": nil,
+		})
+		return
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		log.Println("Open file error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "Unable to read file contents : " + err.Error(),
+			"data": nil,
+		})
+		return
+	}
+	defer src.Close()
+
+	content, err := io.ReadAll(src)
+	if err != nil {
+		log.Println("Read file error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "Unable to read file contents : " + err.Error(),
+			"data": nil,
+		})
+		return
+	}
+
+	question, err := oc.openaiServe.GenerateQuestions(string(content))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "Generate question failed : " + err.Error(),
+			"data": nil,
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"code": 200,
 		"msg":  "ok",
 		"data": gin.H{
 			"filename": file.Filename,
+			"size":     file.Size,
+			"content":  question,
 		},
 	})
 }
--- internal/app/services/openai_service.go
+++ internal/app/services/openai_service.go
@@ -1,5 +1,31 @@
 package services
 
+import (
+	"QuizPals-Server/config"
+	"QuizPals-Server/utils/open_ai"
+)
+
 type OpenAIService interface {
-	UploadFile() error
+	GenerateQuestions(context string) ([]open_ai.Question, error)
+}
+
+type OpenAIServiceImpl struct {
+	Client *open_ai.OpenAIClient
+}
+
+func NewOpenAIService() OpenAIService {
+	cfg := config.GetConfig().OpenAI
+
+	openAiClient := open_ai.NewOpenAIClient(cfg.APIKey, cfg.BaseURL)
+	return OpenAIServiceImpl{
+		Client: openAiClient,
+	}
+}
+
+func (os OpenAIServiceImpl) GenerateQuestions(context string) ([]open_ai.Question, error) {
+	quest, err := os.Client.StartSession(context)
+	if err != nil {
+		return []open_ai.Question{}, err
+	}
+	return quest, nil
 }
